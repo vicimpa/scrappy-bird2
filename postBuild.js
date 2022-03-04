c501cc7d0c200598cae56b67c983ea98aa6d65b3
PromiSe#### <vicimpa@ya.ru>
2022-03-04 14:16:46 +0300

    Remove old push

--- postBuild.js
+++ postBuild.js
@@ -3,5 +3,5 @@ const { readFileSync, rmSync, writeFileSync } = require('fs')
 rmSync('./dist/assets', { recursive: true, force: true })
 const fileData = readFileSync('./dist/index.html', 'utf-8')
 
-writeFileSync('./dist/index.ord.html', fileData)
+// writeFileSync('./dist/index.ord.html', fileData)
 writeFileSync('./dist/index.html', fileData.replace(/\/\*([^\/]+)\*\//gsm, ''))
